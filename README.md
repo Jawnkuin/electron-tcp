b10b5d828a9c89d1259a28d300709371e46210db
Joseph Ku <jawnkuin@gmail.com>
2017-03-25 02:09:36 +0800

    lint & readme

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
-# electron-quick-start
+# electron-tcp
 
 **Clone and run for a quick way to see an Electron in action.**
 
 This is a minimal Electron application based on the [Quick Start Guide](http://electron.atom.io/docs/latest/tutorial/quick-start) within the Electron documentation.
-
-**Use this app along with the [Electron API Demos](http://electron.atom.io/#get-started) app for API code examples to help you get started.**
-
-A basic Electron application needs just these files:
-
-- `package.json` - Points to the app's main file and lists its details and dependencies.
-- `main.js` - Starts the app and creates a browser window to render HTML. This is the app's **main process**.
-- `index.html` - A web page to render. This is the app's **renderer process**.
-
-You can learn more about each of these components within the [Quick Start Guide](http://electron.atom.io/docs/latest/tutorial/quick-start).
 
 ## To Use
 
@@ -20,12 +10,16 @@ To clone and run this repository you'll need [Git](https://git-scm.com) and [Nod
 
 ```bash
 # Clone this repository
-git clone https://github.com/electron/electron-quick-start
+git clone https://github.com/Jawnkuin/electron-tcp
 # Go into the repository
-cd electron-quick-start
+cd electron-tcp
 # Install dependencies
 npm install
-# Run the app
+
+# open another terminal type below to listen port 9090
+nc -l 9090
+
+# Run the app in the fisrt terminal
 npm start
 ```
 
--- renderer.js
+++ renderer.js
@@ -1,10 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var net = require('net');
+const net = require('net');
 
 let client = null; // node socket
-let socketId; // chrome API socket id
+// chrome API socket id
 
 function toBuffer(ab) {
   return new Buffer(new Uint8Array(ab));
@@ -14,44 +14,36 @@ function toArrayBuffer(buf) {
   return new Uint8Array(buf).buffer;
 }
 
-function initConnToServer (ip, port) {
-  return new Promise((resolve, reject) => {
-    if(typeof chrome !== 'undefined') {
-      chrome.sockets.tcp.create({}, r => {
-        socketId = r.socketId;
-        chrome.sockets.tcp.connect(r.socketId, ip, port, code => resolve(code));
-      });
-    } else {
+function initConnToServer(ip, port) {
+  return new Promise((resolve) => {
+    if (!client) {
       client = new net.Socket(); // return a Node socket
+    }
+    if (!client.connecting) {
       client.connect(port, ip);
       client.on('connect', () => resolve());
     }
   });
-};
+}
 
-function sendToServer_simple (data) {
-  return new Promise((resolve, reject) => {
-    if(typeof chrome !== 'undefined') {
-      console.log('chrome');
-      chrome.sockets.tcp.send(socketId, data, r => {});
-      chrome.sockets.tcp.onReceive.addListener(receiveInfo => resolve(receiveInfo.data));
-    } else {
-      console.log('node');
-      client.write(toBuffer(data));
-      client.on('data', data => resolve(toArrayBuffer(data)));
-    }
+function sendToServerSimple(data) {
+  return new Promise((resolve) => {
+    console.log('node');
+    client.write(data);
+    client.on('data', res => resolve(res));
   });
-};
+}
 
-function sendTcp (){
+async function sendTcp() {
   console.log('ini');
-  initConnToServer('192.168.8.29',9090).then(() => sendToServer_simple('Hello!').then(
-      (data) => {console.log(data);}
-    )
-  )
-};
+  initConnToServer('127.0.0.1', 9090).then(() => {
+    sendToServerSimple('Hello!').then((res) => {
+      console.log(res.toString());
+    });
+  });
+}
 
 const sendBtn = document.getElementById('send');
-sendBtn.addEventListener('click', function (event) {
+sendBtn.addEventListener('click', () => {
   sendTcp();
 });
