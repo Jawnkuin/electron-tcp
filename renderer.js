19d83817091da04172064f06751720a346e63467
joseph <jawnkuin@gmail.com>
2017-06-13 12:05:07 +0800

    child process tcp

--- renderer.js
+++ renderer.js
@@ -1,49 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const net = require('net');
-
-let client = null; // node socket
-// chrome API socket id
-
-function toBuffer(ab) {
-  return new Buffer(new Uint8Array(ab));
-}
-
-function toArrayBuffer(buf) {
-  return new Uint8Array(buf).buffer;
-}
-
-function initConnToServer(ip, port) {
-  return new Promise((resolve) => {
-    if (!client) {
-      client = new net.Socket(); // return a Node socket
-    }
-    if (!client.connecting) {
-      client.connect(port, ip);
-      client.on('connect', () => resolve());
-    }
-  });
-}
-
-function sendToServerSimple(data) {
-  return new Promise((resolve) => {
-    console.log('node');
-    client.write(data);
-    client.on('data', res => resolve(res));
-  });
-}
-
-async function sendTcp() {
-  console.log('ini');
-  initConnToServer('127.0.0.1', 9090).then(() => {
-    sendToServerSimple('Hello!').then((res) => {
-      console.log(res.toString());
-    });
-  });
-}
+const electron = require('electron');
+// Module to control application life.
+const ipcRenderer = electron.ipcRenderer;
 
 const sendBtn = document.getElementById('send');
+const textInput = document.getElementById('text');
+
 sendBtn.addEventListener('click', () => {
-  sendTcp();
+  if (textInput.value) {
+    ipcRenderer.send('SEND_DATA', textInput.value);
+  }
 });
